b87b93b8b163793a688f624176ff6c6f7beb7b9c
沈友雄 <2285675743@qq.com>
2022-02-22 17:15:59 +0800

    第一次使用idea commit的本地

---
+++ src/main/java/com/tiheima/Girl.java
@@ -0,0 +1,18 @@
+package com.tiheima;
+
+/**
+ * @author youxiong Shen
+ * @ClassName Girl
+ * @Description
+ * @date 17:10 2022-2-22
+ * @Version 2.1
+ */
+public class Girl {
+
+    private String name;
+
+    private String age;
+
+
+
+}
